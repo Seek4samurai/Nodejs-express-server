4e398001429438e688587bd1ac8bb9bff8f865f1
Seek4samurai <grvrawat999@gmail.com>
2022-07-14 16:25:03 +0530

    chore: added writeHead

--- Part-1/nodeServer.js
+++ Part-1/nodeServer.js
@@ -4,9 +4,12 @@ const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-type", "text/plain");
-  res.end("Hello World");
+  // res.statusCode = 200;
+  // res.setHeader("Content-type", "text/plain");
+  // res.end("Hello World");
+
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ data: "Hello World!" }));
 });
 
 server.listen(port, hostname, () => {
